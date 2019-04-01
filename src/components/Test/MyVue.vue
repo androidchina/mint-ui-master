201f356c686cdfdfb7f6efb6de8423d76e59c72d
xiaoli100861 <1720001895@qq.com>
2019-04-01 09:33:32 +0800

    Initial commit

---
+++ src/components/Test/MyVue.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!--<gheader :examplename="examplename"></gheader>-->
+    <ul>
+      <li><mt-button size="large" @click="ToastA">Toast111</mt-button></li>
+      <li><mt-button size="large" @click="ToastB">Toast222</mt-button></li>
+      <li><mt-button size="large" @click="ToastC">Toast333</mt-button></li>
+    </ul>
+    <!--<gfooter></gfooter>-->
+  </div>
+</template>
+<script>
+  import { Toast } from 'mint-ui';
+  export default {
+    name: 'MyVue',
+    data(){
+      return {
+        examplename: "MyVue"
+      }
+    },
+    mounted(){
+
+    },
+    methods:{
+      ToastA(){
+        Toast('默认Toast');
+      },
+      ToastB(){
+        Toast({
+          message: '操作成功',
+          iconClass: 'fa fa-check fa-2x'
+        });
+      },
+      ToastC(){
+        Toast({
+          message: '自定义位置',
+          position: 'bottom',
+          duration: 5000
+        });
+      }
+    }
+  }
+</script>
+<style scoped>
+  ul {
+    padding-left: 10px;
+    padding-top: 20px;
+    padding-right: 10px;
+  }
+  li {
+    margin: 20px 0;
+  }
+</style>
+
